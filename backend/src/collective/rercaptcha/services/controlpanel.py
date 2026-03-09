ab26cc721d778f74474442fadfe2e730343a200f
Federico Vancini <federico.vancini@redturtle.it>
2026-03-09 10:16:45 +0100

    Configured controlpanel for rest API

---
+++ backend/src/collective/rercaptcha/services/controlpanel.py
@@ -0,0 +1,15 @@
+from collective.rercaptcha.controlpanels.controlpanel import IRerCaptchaSettings
+from collective.rercaptcha.interfaces import IBrowserLayer
+from plone.restapi.controlpanels import RegistryConfigletPanel
+from zope.component import adapter
+from zope.interface import implementer
+from zope.interface import Interface
+
+
+@adapter(Interface, IBrowserLayer)
+@implementer(IRerCaptchaSettings)
+class RerCaptchaSettingsControlpanel(RegistryConfigletPanel):
+    schema = IRerCaptchaSettings
+    configlet_id = "RerCaptchaSettings"
+    configlet_category_id = "Products"
+    schema_prefix = None
